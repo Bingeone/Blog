286502dc91da1240d2775068ceffe2aa171ff5e0
Bingeone <wangjingbi57@163.com>
2021-07-15 19:17:12 +0800

    登录界面子应用和项目设置配置

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 子应用的注册
+    'users.apps.UsersConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'  # en-us
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'   # UTC
 
 USE_I18N = True
 
@@ -122,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 设置静态资源的路径
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # redis配置
 
@@ -146,3 +154,49 @@ CACHES = {
 # session由数据库存储改成redis
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "session"
+
+# 项目日志设置
+# 日志
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,  # 是否禁用已经存在的日志器
+    'formatters': {  # 日志信息显示的格式
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            'format': "%(levelname)s %(module)s %(lineno)d %(message)s"
+        },
+    },
+    'filters': {  # 对日志进行过滤
+        'require_debug_true': {  # django在debug模式下才输出日志
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {  # 日志处理方法
+        'console': {  # 像终端输出日志
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            # 'encoding': 'utf-8',  # 设置默认编码
+        },
+        'file': {  # 向文件中输出日志
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/blog.log'),  # 日志文件的位置
+            'maxBytes': 300 * 1024 * 1024,
+            'backupCount': 10,
+            'formatter': 'verbose',
+            'encoding': 'utf-8',  # 设置默认编码
+        },
+    },
+    'loggers': {  # 日志器
+        'django': {  # 定义了一个名为django的日志器
+            'handlers': ['console', 'file'],  # 可以同时向终端与文件中输出日志
+            'propagate': True,  # 是否继续传递日志信息
+            'level': 'INFO',  # 日志器接受的最低日志级别
+            'encoding': 'utf-8',  # 设置默认编码
+        }
+    },
+}
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# 1、导入系统的logging
+import logging
+
+# 2、创建获取日志器
+# logger = logging.getLogger('django')
+#
+# from django.http import HttpResponse
+#
+#
+# def log(request):
+#     # 3、使用日志器记录信息
+#     logger.info('这是输入信息测试')
+#     return HttpResponse('test')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # include的参数中，包括一个元组 urlconf_module,app_name    namespace命名空间
+    path('', include(('users.urls', 'users'), namespace='users'))
+    # path('', log)
 ]
